a4010ab33f20bd7d33c2466e2361f378108117c6
mominur-cloud <83661855+mominur-cloud@users.noreply.github.com>
2024-08-27 20:15:03 +1000

    eks role and subnets

--- infra/team-a/dev/main.tf
+++ infra/team-a/dev/main.tf
@@ -1,9 +1,18 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0"
-    }
-  }
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+
+module "k8s-role" {
+source = "../../../modules/k8s/iam"
+eks_role = "cluster-role"
+}
+
+module "eks" {
+  source = "../../../modules/k8s/baseline"
+  depends_on = [ module.k8s-role ]
+  name     = "eks-dev"
+  role_arn = module.k8s-role.role_arn
+  subnets = ["subnet-0d0862007c7f182dd", "subnet-05f217cb97ba8768c"]
 }
 
